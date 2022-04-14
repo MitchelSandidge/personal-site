5525242cfbb7d51ed4d72045efc26e3d323d0a5d
Mitchel Sandidge <msandidge@appddictionstudio.com>
2022-04-14 10:46:06 -0500

    Cleaned up before changes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import'../../App.css';
 import HeroSection from '../HeroSection';
 import Cards from '../Cards';
-import Footer from '../Footer';
 
 function Home() {
     return (
